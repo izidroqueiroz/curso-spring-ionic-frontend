cdbfc15f274ca9995723649b64f5de97865bf717
Izidro Queiroz <izidro.queiroz@gmail.com>
2018-12-16 18:55:48 -0200

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+ /*   baseUrl: "https://curso-spring-ionic-izidro.herokuapp.com/", */
+    baseUrl: "https://curso-spring-ionic-izidro.herokuapp.com",
     bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-izidro-queiroz"
 }
